36e0bd02376f4aa3afe1e019980d7bf25c64ca74
Daniel <107085390+Daniel-Alves27@users.noreply.github.com>
2022-11-28 09:46:24 -0300

    Desafio 2 concluido

--- 7DayJavascript.js
+++ 7DayJavascript.js
@@ -1,6 +1,6 @@
 //Desafio #01
 
-const valoresNumero = [1,30,10];
+/*const valoresNumero = [1,30,10];
 
 const valoresString = ['1','30','10'];
 
@@ -14,6 +14,22 @@ function compararValores(valoresNumero,valoresString){
     }
 };
 
-console.log(verificar);
+console.log(verificar);*/
 
 //____________________________________
+
+let nome = prompt("Qual o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+let infoUsuario = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`;
+
+console.log(infoUsuario);
+
+let opcoes = prompt`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`;
+
+if (opcoes == 1) {
+    console.log("Muito bom! Continue estudando e você terá muito sucesso.")
+}else (opcoes == 2);{
+    console.log("Ahh que pena... Já tentou aprender outras linguagens?")
+};
