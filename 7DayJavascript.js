0f8e8ecff4a37230ecc50e458adfd40ad3c9dcf2
Daniel <107085390+Daniel-Alves27@users.noreply.github.com>
2022-12-02 19:54:19 -0300

    desafio 3 e 4 completo

--- 7DayJavascript.js
+++ 7DayJavascript.js
@@ -17,7 +17,9 @@ function compararValores(valoresNumero,valoresString){
 console.log(verificar);*/
 
 //____________________________________
+//Desafio #02
 
+/*
 let nome = prompt("Qual o seu nome?");
 let idade = prompt("Quantos anos você tem?");
 let linguagem = prompt("Qual linguagem de programação você está estudando?");
@@ -33,3 +35,67 @@ if (opcoes == 1) {
 }else (opcoes == 2);{
     console.log("Ahh que pena... Já tentou aprender outras linguagens?")
 };
+*/
+
+//_________________________________________
+//Desafio #03
+
+/*const area = prompt("Você quer seguir para área de Front-end ou seguir para a área de Back-end? Digite a área de preferência.");
+let linguagem = "";
+
+if (area == "Front-end"){
+    linguagem = prompt("Você quer aprende React ou Vue?");
+}
+else if(area == "Back-end"){
+    linguagem = prompt("Você quer aprende C# ou Java?");
+}
+else{
+    alert("Nenhuma informação foi inserida!");
+};
+
+const especializarOuFullstack = prompt(`Você quer se especializar em ${linguagem} ou se tornar Fullstack?Digite 1 para especializar ou 2 para Fullstack.`);
+
+if(especializarOuFullstack == 1 ){
+    alert(`Parabéns continue se especializando em ${linguagem} para dominar a área de ${area}!`);
+}
+else if(especializarOuFullstack == 2){
+    alert(`Que tal aprender um nova linguagem ou framework para ser tornar um Dev Fullstack`);
+}
+else{
+    alert("Nenhuma informação foi inserida!");
+};
+
+let msg = prompt("Qual outra tecnologia você gostaria de se especializar?Digite 'ok' para me dizer qual é");
+while (msg === "ok"){
+    let novaTecnologia = prompt("Qual?");
+    alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`);
+    msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender?Digite 'ok' para me dizer qual é");
+};
+*/
+
+//_________________________________________
+
+//Desafio #04
+
+let numeroAleatorio = Math.floor(Math.random()*11);
+let errouNumero = 0;
+
+console.log(numeroAleatorio)
+for(let i = 0; i < 3; i+=1){
+    adivinharValor = prompt("Qual o valor que você deseja chutar?");
+    //console.log(i);
+    if(numeroAleatorio == adivinharValor){
+        alert(`Parabéns!Você acertou.`); 
+        break;
+    }
+    else if(errouNumero == 3){
+        alert(`Que pena você errou.O número era ${numeroAleatorio}`)
+    }
+    else(numeroAleatorio !== adivinharValor);{
+        errouNumero += 1;
+        //console.log(errouNumero)
+        alert("tente novamente")
+    }
+    
+}
+
